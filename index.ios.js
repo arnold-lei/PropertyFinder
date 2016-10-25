46b81d51c234285f72f3aeb19bcd77fa98157f43
Arnold Lei <arnold.lei88@gmail.com>
2016-10-25 14:45:26 -0400

    added search page removed hello world

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,9 @@
-
 'use strict';
 
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
 
-var styles = ReactNative.StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  },
-  container: {
-    flex: 1
-  }
-});
-
-
-class HelloWorld extends React.Component {
-  render() {
-    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
-  }
-}
 
 class PropertyFinderApp extends React.Component {
   render() {
@@ -30,7 +12,7 @@ class PropertyFinderApp extends React.Component {
         style={styles.container}
         initialRoute={{
           title: 'Property Finder',
-          component: HelloWorld,
+          component: SearchPage3,
         }}/>
     );
   }
