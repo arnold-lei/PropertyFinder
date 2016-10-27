eb3e0de3f721b6b5fc659e99c01a4134a09fbe97
Arnold Lei <arnold.lei88@gmail.com>
2016-10-26 20:58:35 -0400

    working on displaying results

---
+++ SearchResults.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Image,
+    View,
+    TouchableHighlight,
+    ListView,
+    Text
+} from 'react-native';
+
+class SearchResults extends React.Component {
+    constructor(props){
+        super(proprs);
+        var dataSource = new ListView.DataSource (
+            {rowHasChanged: (r1, r2) => r1.lister_url !==  r2.lister_url});
+        this.state = {
+            dataSource: dataSource.clowWithRows(this.props.listings)
+        };
+    }
+    renderRow(rowData, sectionID, rowID){
+        return(
+            <TouchableHighlight
+                underlayColor="#ddd" >
+                <View>
+                    <Text>{rowData.title} </Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+    render(){
+        return(
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRow.bind(this)}/>
+        )
+    }
+}
